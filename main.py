4ba960b3e46380e212b2957df862aa18e4696e95
polakrit krajaisri <117198477+polakrit-pipi@users.noreply.github.com>
2023-05-03 22:50:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class OCR:
     
     def extrair_texto(self, img):
         #O comando que extrai o texto da imagem
-        texto = pytesseract.image_to_string(img, lang="por")
+        texto = pytesseract.image_to_string(img, lang="tha")
         return texto
     
     def mostrar_analise(self):
