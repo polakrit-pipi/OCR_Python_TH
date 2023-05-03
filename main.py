516a97bd4021a0de4281211727bb29024dd06b03
polakrit krajaisri <117198477+polakrit-pipi@users.noreply.github.com>
2023-05-03 23:07:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,24 +7,24 @@ class OCR:
 
     def __init__(self):
         st.set_page_config(page_title="Python OCR")
-        self.texto = ""
+        self.textto = ""
 
     def inicial(self):
-        st.title("OCR Programa")
+        st.title("OCR Program")
         st.write("Optical Character Recognition (OCR) Python")
-        imagem = st.file_uploader("Select file type", type=["png","jpg"])
-        if imagem:
-            img = Image.open(imagem)
+        imageme = st.file_uploader("Select file type", type=["png","jpg"])
+        if imageme:
+            img = Image.open(imageme)
             st.image(img, width=350)
             st.info("Text transform")
-            self.texto = self.extract_img(img)
-            st.write("{}".format(self.texto))
+            self.textto = self.extract_img(img)
+            st.write("{}".format(self.textto))
             
       
     
     def extract_img(self, img):
-        texto = pytesseract.image_to_string(img, lang="tha+eng")
-        return texto
+        textto = pytesseract.image_to_string(img, lang="tha+eng")
+        return textto
     
    
 ocr = OCR()
