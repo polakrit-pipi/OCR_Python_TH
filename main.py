e84efc93e39f3b652a298e865ab847ed4c8c315a
polakrit krajaisri <117198477+polakrit-pipi@users.noreply.github.com>
2023-05-03 23:03:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
-#import principal
 import streamlit as st
-#imports relacionados
 from PIL import Image
 import pytesseract
-#metodos internos
 
 
 class OCR:
 
     def __init__(self):
-        #altera titulo da pagina
         st.set_page_config(page_title="Python OCR")
-        #inicializa variveis
         self.texto = ""
-        self.analisar_texto = False
 
     def inicial(self):
-        #conteudo inicial da pagina
         st.title("OCR Programa")
-        st.write("Optical Character Recognition (OCR) implementado com Python")
-        imagem = st.file_uploader("Selecione alguma imagem", type=["png","jpg"])
-        #se selecionar alguma imagem...
+        st.write("Optical Character Recognition (OCR) Python")
+        imagem = st.file_uploader("Select file type", type=["png","jpg"])
         if imagem:
             img = Image.open(imagem)
             st.image(img, width=350)
-            st.info("Texto extraído")
-            self.texto = self.extrair_texto(img)
+            st.info("Text transform")
+            self.texto = self.extract_img(img)
             st.write("{}".format(self.texto))
             
-            #Opcao de analisar texto
-            self.analisar_texto = st.sidebar.checkbox("Analisar texto")
-            if self.analisar_texto==True:
-                self.mostrar_analise()
+      
     
-    def extrair_texto(self, img):
-        #O comando que extrai o texto da imagem
+    def extract_img(self, img):
         texto = pytesseract.image_to_string(img, lang="tha+eng")
         return texto
     
